050176ee57b60fbc3b69be030ed3d9b64bf5fa4a
yiwiy9 <yiwiy9@gmail.com>
2022-04-15 19:48:18 +0900

    Fix for api

---
+++ docker/backend/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 秘密情報をビルドに含めない
+# serverless framework にAWSアカウントを紐付ける
+serverless config credentials \
+    --provider aws \
+    --key $AWS_ACCESS_KEY_ID \
+    --secret $AWS_SECRET_ACCESS_KEY &&
+
+# 依存パッケージのインストール
+cd api && npm install && pip install -r requirements.txt &&
+
+# exitしないためにベースイメージの`CMD`実行
+python
