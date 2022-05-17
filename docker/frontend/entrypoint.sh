c6f99d67e63ec1822f4964cf0fd53275ad2691d0
yiwiy9 <yiwiy9@gmail.com>
2022-05-18 00:21:41 +0900

    Fix frontend

--- docker/frontend/entrypoint.sh
+++ docker/frontend/entrypoint.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# 秘密情報をビルドに含めない
-# serverless framework にAWSアカウントを紐付ける
-serverless config credentials \
-    --provider aws \
-    --key $AWS_ACCESS_KEY_ID \
-    --secret $AWS_SECRET_ACCESS_KEY &&
-
 # 依存パッケージのインストール
-yarn
+yarn &&
 
-# アプリの立ち上げ
-yarn dev
+# exitしないためにベースイメージの`CMD`実行
+node
